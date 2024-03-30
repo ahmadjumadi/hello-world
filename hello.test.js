05bfc26e946d739bec44e31bd18b43562d9646a4
falathin <ibrahimahmadfalatin@gmail.com>
2024-03-30 11:39:33 +0700

    tambah wkwkwk

--- hello.test.js
+++ hello.test.js
@@ -1,6 +1,6 @@
 const hello = require('./hello');
 
 test('outputs the correct string', () => {
-  expect(hello()).toBe("Hello world!");
+  expect(hello()).toBe("Hello world wkwkwk!");
   expect(soalKedua()).toBe(10);
 });
